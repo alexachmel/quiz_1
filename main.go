cf4c09d95cc227163d815618652a39cbd6f38e25
Oleksandra Chmel <alexa.chmel@gmail.com>
2021-11-09 20:43:04 +0200

    create main package

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Init the model for json data
+/*
+type Quiz struct {
+    Question string 'json: "question"'
+    Answer string 'json: "answer"'
+}
+*/
+
+func main() {
+	fmt.Println("1 + 1 =", 1+1)
+}
